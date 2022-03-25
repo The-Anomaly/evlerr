38f8876fb7acb214221c9516261964a92e37f436
Bobby <bobby@Paylots-MacBook-Pro.local>
2022-03-25 12:12:22 +0100

    fix:update fixes for netlify deploy

--- src/components/landingPage/MiniDropDown.js
+++ src/components/landingPage/MiniDropDown.js
@@ -1,41 +1,4 @@
-// import React from 'react';
-// import Box from '@mui/material/Box';
-// import InputLabel from '@mui/material/InputLabel';
-// import FormControl from '@mui/material/FormControl';
-// import Select from '@mui/material/Select';
-
-// const MiniDropDown = ({ label, children }) => {
-
-//     const [age, setAge] = React.useState('');
-
-//     const handleChange = (event) => {
-//         setAge(event.target.value);
-//     };
-//     return (
-//         <>
-//             <Box sx={{ minWidth: 120 }}>
-//                 <FormControl fullWidth>
-//                     <InputLabel id="demo-simple-select-label">{label}</InputLabel>
-//                     <Select
-//                         labelId="demo-simple-select-label"
-//                         id="demo-simple-select"
-//                         value={age}
-//                         label={label}
-//                         onChange={handleChange}
-//                     >
-//                         {children}
-//                     </Select>
-//                 </FormControl>
-//             </Box>
-//         </>
-//     )
-// }
-
-// export default MiniDropDown
-
-
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
