3f9d65dcf2ee5418a28244d1e5f22802130b4f60
charles-ekube <charleswakili797@gmail.com>
2022-04-10 17:39:02 +0100

    fix:netlify deploy fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import { Suspense, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/Index';
 import RoutesContainer from './route/Routes';
-import Loading from './utils/Loading';
 
 
 function App() {
 
-  useEffect(() => {
-    setTimeout(() => {
-      <Loading />
-    }, 7000)
-  }, [<Loading />])
 
 
   return (
     <Provider store={store}>
-      <Suspense fallback={'Loading...'}>
-        <RoutesContainer />
-      </Suspense>
+      <RoutesContainer />
     </Provider>
 
   );
