0667a46e3dff9be368d33a6b6b51d361ea0d5b51
muritz <mauricechisom275@gmail.com>
2022-05-17 16:58:29 +0100

    fix: redux devtool issue

--- src/redux/Index.js
+++ src/redux/Index.js
@@ -3,6 +3,8 @@ import thunk from 'redux-thunk';
 
 import reducers from './reducer/Index';
 
-const store = createStore(reducers, {}, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(reducers, {}, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__
+? window.__REDUX_DEVTOOLS_EXTENSION__()
+: f => f))
 
 export default store;
