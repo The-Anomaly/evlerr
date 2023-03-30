aaaf286aaf3ab371cad2677e41966536473105a2
Favour Adekogbe <favouradekogbe@gmail.com>
2023-03-31 00:27:38 +0100

    change api url

--- src/Utils.js
+++ src/Utils.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 import qs from 'qs';
 import axios from "axios";
-const apiRootUrl = 'https://evlerr-api.herokuapp.com/api/v1/'
+const apiRootUrl = 'https://evlerr-production.up.railway.app'
 
 
 const getUserToken = async () => {
